3884214da947d7cba4769f3f438564e7f6846bf6
Jagannath8 <64650848+Jagannath8@users.noreply.github.com>
2023-01-07 15:00:10 +0530

    Q-134 (Day-41)

---
+++ Gas Station.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum=0, total=0, pos=0;
+        for(int i=0;i<gas.length;i++){
+            sum+=gas[i]-cost[i];
+            if(sum<0){
+                total+=sum;
+                sum=0;
+                pos=i+1;
+            }
+        }
+        total+=sum;
+
+        if(total>=0)
+            return pos;
+        else 
+            return -1;
+    }
+}
