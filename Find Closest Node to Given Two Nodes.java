97032f168d5d89c0454299012d92061d25ae1fc9
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-01-26 01:12:17 +0530

    Q-2359 (Day-59)

---
+++ Find Closest Node to Given Two Nodes.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int n=edges.length;
+        int dist1[]=new int[n];
+        int dist2[]=new int[n];
+        Arrays.fill(dist1, -1);
+        Arrays.fill(dist2, -1);
+
+        dist1[node1]=0;
+        dist2[node2]=0;
+
+        int dist=0;
+        int i=node1;
+
+        while(true){
+            dist1[i]=dist++;
+            i=edges[i];
+            if(i==-1 || dist1[i]!=-1)
+                break;
+        }
+
+        dist=0;
+        i=node2;
+
+        while(true){
+            dist2[i]=dist++;
+            i=edges[i];
+            if(i==-1 || dist2[i]!=-1)
+                break;
+        }
+
+        int ans=-1;
+        int mini=Integer.MAX_VALUE;
+
+        for(i=0;i<n;i++){
+            if(dist1[i]==-1 || dist2[i]==-1)
+                continue;
+
+            if(mini>Math.max(dist1[i], dist2[i])){
+                mini=Math.max(dist1[i], dist2[i]);
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
