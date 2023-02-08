83c4524bcc580aeb29b66d39e15d39d78a1ba35f
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-02-09 02:10:21 +0530

    Q-45 (Day-73)

---
+++ Jump Game II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int jump(int[] nums) {
+        int far=0, end=0, ans=0;
+        for(int i=0;i<nums.length-1;i++){
+            far=Math.max(far, i+nums[i]);
+            if(i==end){
+                ans++;
+                end=far;
+            }
+        }
+        return ans;
+    }
+}
