e6266b91580e1cdc711afc257dbfc766b93540fb
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-05-16 02:16:00 +0530

    Q-1721 (Day-169)

---
+++ Swapping Nodes in a Linked List.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode left=head, right=head;
+        for(int i=1;i<k;i++){
+            left=left.next; 
+        }
+
+        ListNode curr=left;
+        while(curr.next!=null){
+            curr=curr.next;
+            right=right.next;
+        }
+
+        int temp=left.val;
+        left.val=right.val;
+        right.val=temp;
+        return head;
+    }
+}
