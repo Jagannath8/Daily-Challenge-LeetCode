49ad4d7827ffbcfe9fc41c6d5038a5c6d598514d
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-06-15 03:19:59 +0530

    Q-530 (Day-199)

---
+++ Minimum Absolute Difference in BST.java
@@ -0,0 +1,21 @@
+class Solution {
+    int ans=Integer.MAX_VALUE;
+    TreeNode prev=null;
+    
+    public int getMinimumDifference(TreeNode root) {
+        inOrder(root);
+        return ans;
+    }
+
+    private void inOrder(TreeNode root){
+        if(root==null)
+            return;
+
+        inOrder(root.left);
+        if(prev!=null){
+            ans=Math.min(ans, root.val-prev.val);
+        }
+        prev=root;
+        inOrder(root.right);
+    }
+}
