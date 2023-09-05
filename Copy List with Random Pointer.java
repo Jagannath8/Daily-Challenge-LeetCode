214a18037769111454bcbfe70c172242b30431dd
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-09-06 01:08:33 +0530

    Q-138 (Day-282)

---
+++ Copy List with Random Pointer.java
@@ -0,0 +1,16 @@
+public class Solution {
+    Map<Node, Node> map = new HashMap<>();
+    public Node copyRandomList(Node head) {
+        if (head == null) 
+            return null;
+
+        if (map.containsKey(head)) 
+            return map.get(head);
+
+        Node newNode = new Node(head.val);
+        map.put(head, newNode);
+        newNode.next = copyRandomList(head.next);
+        newNode.random = copyRandomList(head.random);
+        return newNode;
+    }
+}
