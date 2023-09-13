4d6d783834f8ad94930a16e11b01e4c4826c557f
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-09-14 01:14:42 +0530

    Q-135 (Day-290)

---
+++ Candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int ans=0, n=ratings.length;
+        int left[]=new int[n];
+        int right[]=new int[n];
+
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                left[i]=left[i-1]+1;
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1])
+                right[i]=right[i+1]+1;
+        }
+
+        for(int i=0;i<n;i++){
+            ans=ans+Math.max(left[i], right[i]);
+        }
+
+        return ans;
+    }
+}
