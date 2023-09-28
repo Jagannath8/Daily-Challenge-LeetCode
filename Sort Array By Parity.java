ff5e88d4f2729f61ec14f5953275c6b46f4926c6
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-09-29 01:19:12 +0530

    Q-905 (Day-305)

---
+++ Sort Array By Parity.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int i=0;
+        int j=nums.length-1; 
+        while (i<j) {
+            while (i<j && nums[i]%2==0)
+                i++;
+            while (i<j && nums[j]%2==1)
+                j--;
+            
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        } 
+        return nums;
+    }
+}
