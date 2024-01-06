f489e880ddb25f28c9894c260a325a68cb4b2f24
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2024-01-07 01:45:25 +0530

    Q-1235 (Day-405)

---
+++ Maximum Profit in Job Scheduling.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n=startTime.length;
+        int jobs[][]=new int[n][3];
+        for(int i=0;i<n;i++){
+            jobs[i]=new int[]{startTime[i], endTime[i], profit[i]};
+        }
+        Arrays.sort(jobs, (a,b)->a[1]-b[1]);
+        TreeMap<Integer, Integer> dp=new TreeMap<>();
+        dp.put(0,0);
+        for(int j[]:jobs){
+            int val=j[2]+dp.floorEntry(j[0]).getValue();
+            if(val>dp.lastEntry().getValue()){
+                dp.put(j[1], val);
+            }
+        }
+        return dp.lastEntry().getValue();
+    }
+}
